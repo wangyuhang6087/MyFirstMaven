77318f0f089c7893151fa51be5d49225f6df8896
wangyuhang6087 <wangyuhangking1@163.com>
2019-10-12 19:18:27 +0800

    提交MyFirstMaven

---
+++ src/main/java/com/zhiyou100/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package com.zhiyou100.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import com.zhiyou100.model.User;
+import org.springframework.stereotype.Repository;
+
+
+public interface UserMapper {
+
+	List<User> findAll(Map<String, Object> map);
+
+	int addUser(User user);
+
+	int count(Map<String, Object> map);
+
+	User findUserById(int id);
+
+	List<Integer> findAllIds();
+
+	void deleteUserById(int id);
+
+	void updateUser(User user);
+
+}
